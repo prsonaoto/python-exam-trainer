b34d21743cf59b8885d2cc60dcd67c6ab64dd186
prsonaoto <112635170+prsonaoto@users.noreply.github.com>
2026-09-11 10:06:47 +0900

    Load v6.2 targeted question-bank update

--- data/fixes.js
+++ data/fixes.js
@@ -1,3 +1,4 @@
 window.QUESTION_BANK=window.QUESTION_BANK||[];
 document.write('<script src="./data/v6.js"><\/script>');
 document.write('<script src="./data/v6-1.js"><\/script>');
+document.write('<script src="./data/v6-2.js"><\/script>');
