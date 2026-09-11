bae218d3be87ba89c710358c197a543bbc55af7c
prsonaoto <112635170+prsonaoto@users.noreply.github.com>
2026-09-11 10:36:49 +0900

    Load v6.3 NumPy and pandas extension

--- data/fixes.js
+++ data/fixes.js
@@ -2,3 +2,4 @@ window.QUESTION_BANK=window.QUESTION_BANK||[];
 document.write('<script src="./data/v6.js"><\/script>');
 document.write('<script src="./data/v6-1.js"><\/script>');
 document.write('<script src="./data/v6-2.js"><\/script>');
+document.write('<script src="./data/v6-3.js"><\/script>');
