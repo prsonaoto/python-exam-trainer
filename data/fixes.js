3701c7956c60252b11ddb0a9277f93a24124013d
prsonaoto <112635170+prsonaoto@users.noreply.github.com>
2026-09-11 11:09:25 +0900

    Load v6.4 pandas drop questions

--- data/fixes.js
+++ data/fixes.js
@@ -3,3 +3,4 @@ document.write('<script src="./data/v6.js"><\/script>');
 document.write('<script src="./data/v6-1.js"><\/script>');
 document.write('<script src="./data/v6-2.js"><\/script>');
 document.write('<script src="./data/v6-3.js"><\/script>');
+document.write('<script src="./data/v6-4.js"><\/script>');
